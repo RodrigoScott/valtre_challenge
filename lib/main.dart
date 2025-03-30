1e86ec59644e2c0cb119011958ba996725ca8b7b
RodrigoScott <scott_rona1@hotmail.com>
2025-03-29 21:25:41 -0600

    update: :arrow_up: update path for screens

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:valtre_challege/src/data/utils/constants.dart';
 
-import 'src/presentation/home_page/home_page.dart';
+import 'src/presentation/screens/home_page/home_page.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/src/presentation/screens/home_page/home_page.dart
+++ lib/src/presentation/screens/home_page/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../domain/bloc/pokemon_list/pokemon_cubit.dart';
+import '../../../domain/bloc/pokemon_list/pokemon_cubit.dart';
 import 'home_page_view.dart';
 
 class HomePage extends StatelessWidget {
