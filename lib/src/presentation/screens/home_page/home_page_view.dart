b60db4e23208cc9215b2241df45da1d69e0a8754
RodrigoScott <scott_rona1@hotmail.com>
2025-03-29 21:52:42 -0600

    update: :art: update home body widget and implement custom header and home body widget

--- lib/src/presentation/screens/home_page/home_page_view.dart
+++ lib/src/presentation/screens/home_page/home_page_view.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:valtre_challege/src/presentation/screens/home_page/widgets/widgets.dart';
+
+import '../../../data/utils/constants.dart';
 
 class HomePageView extends StatelessWidget {
   const HomePageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0.0),
+        child: AppBar(backgroundColor: primaryColor),
+      ),
+      body: Column(
+        children: [CustomHeader(), Expanded(child: HomeBodyWidget())],
+      ),
+    );
   }
 }
